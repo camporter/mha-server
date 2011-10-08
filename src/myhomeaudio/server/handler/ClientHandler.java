6178c964484f688c7a96c2be61de077c1369ba2b
camporter <camporter1@gmail.com>
2011-10-08 17:13:18 -0500

    Added http stuff

--- src/myhomeaudio/server/handler/ClientHandler.java
+++ src/myhomeaudio/server/handler/ClientHandler.java
@@ -8,31 +8,36 @@ import java.util.ArrayList;
 import myhomeaudio.server.http.Worker;
 
 /**
- * ClientHandler runs as a thread and waits for Clients to connect through its loop.
+ * ClientHandler runs as a thread and waits for Clients to connect through its
+ * loop.
+ * 
  * @author cameron
- *
+ * 
  */
 public class ClientHandler extends Thread {
 	private ServerSocket clientListenSocket;
 	private int clientListenPort;
-	private ArrayList<Worker> workers; // holds all the workers we can use to service client requests
-	private int numWorkers;
-	
+	private ArrayList<Worker> workerPool; // holds all the workers we can use to
+										// service client requests
+	public final int timeout = 0;
+	private int maxNumWorkers;
+
 	public ClientHandler(int port) {
 		this.clientListenSocket = null;
 		this.clientListenPort = port;
-		this.workers = new ArrayList<Worker>();
-		this.numWorkers = 5;
-		
+		this.workerPool = new ArrayList<Worker>();
+		this.maxNumWorkers = 5;
+
 		try {
 			clientListenSocket = new ServerSocket(this.clientListenPort);
 		} catch (IOException e) {
-			System.out.println("ClientHandler: Unable to bind to port: " + this.clientListenPort);
+			System.out.println("ClientHandler: Unable to bind to port: "
+					+ this.clientListenPort);
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void run() {
 		try {
 			while (true) {
@@ -41,16 +46,29 @@ public class ClientHandler extends Thread {
 				if (this.clientListenSocket == null) {
 					return; // Stop this thread if the socket isn't available
 				}
-				
-				for (int i=0; i<this.numWorkers; ++i) {
-					Worker w = new Worker();
+
+				for (int i = 0; i < this.maxNumWorkers; ++i) {
+					Worker w = new Worker(this);
 					w.start();
-					workers.add(w);
+					workerPool.add(w);
 				}
 
 				Socket clientSocket = this.clientListenSocket.accept();
 				System.out.println("Client connection Found");
-				
+				Worker worker = null;
+				synchronized (workerPool) {
+					if (workerPool.isEmpty()) {
+						// We don't have anymore workers, add a new one to use
+						worker = new Worker(this);
+						worker.setClientSocket(clientSocket);
+						worker.start();
+					} else {
+						// We have a worker -- give it the socket
+						worker = (Worker) workerPool.get(0);
+						workerPool.remove(0);
+						worker.setClientSocket(clientSocket);
+					}
+				}
 
 			}
 		} catch (IOException e) {
@@ -58,5 +76,33 @@ public class ClientHandler extends Thread {
 			System.out.println("NodeHandler exited!");
 			return;
 		}
+	}
+
+	/**
+	 * Attempts to add a Worker to the Worker pool. It won't add another Worker
+	 * if there are enough in the pool already.
+	 * 
+	 * @param worker Worker object to be added.
+	 * @return Whether the Worker was added to the pool.
+	 */
+	synchronized public boolean addWorker(Worker worker) {
+		if (this.getWorkerCount() == getMaxWorkers())
+		{
+			// Already have enough workers!
+			return false;
+		}
+		else {
+			// We have space, let's add this Worker.
+			this.workerPool.add(worker);
+			return true;
+		}
+	}
+
+	public int getWorkerCount() {
+		return this.workerPool.size();
+	}
+
+	public int getMaxWorkers() {
+		return this.maxNumWorkers;
 	}
 }
--- src/myhomeaudio/server/http/Worker.java
+++ src/myhomeaudio/server/http/Worker.java
@@ -1,5 +1,117 @@
 package myhomeaudio.server.http;
 
-public class Worker extends Thread {
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.Socket;
 
+import myhomeaudio.server.handler.ClientHandler;
+
+/**
+ * Worker is a thread that services a client's request.
+ * 
+ * @author cameron
+ * 
+ */
+public class Worker extends Thread {
+	final static int BUF_SIZE = 2048;
+
+	static final byte[] EOL = { (byte) '\r', (byte) '\n' };
+
+	// buffer to use for requests
+	byte[] buf;
+	private Socket clientSocket;
+	private ClientHandler clientHandler;
+
+	public Worker(ClientHandler clientHandler) {
+		this.clientHandler = clientHandler;
+		this.buf = new byte[BUF_SIZE];
+		this.clientSocket = null;
+	}
+
+	// Set the client socket
+	synchronized public void setClientSocket(Socket socket) {
+		this.clientSocket = socket;
+		notify();
+	}
+	
+	synchronized public void run() {
+		while(true) {
+			if (clientSocket == null) {
+				// Don't have a socket yet!
+				try {
+					// Wait for the thread to wake back up
+					wait();
+				} catch (InterruptedException e) {
+					// ???
+					continue;
+				}
+			}
+			
+			try {
+				handleClient();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+			
+			clientSocket = null; // reset the socket for next use
+			
+			// Done using the Worker, put it back in the Worker pool
+			synchronized(clientHandler) {
+				if (!clientHandler.addWorker(this)) {
+					// Worker no longer needed
+					return;
+				}
+			}
+		}
+	}
+
+	private void handleClient() throws IOException {
+		InputStream inputStream = new BufferedInputStream(this.clientSocket.getInputStream());
+		PrintStream printStream = new PrintStream(this.clientSocket.getOutputStream());
+		
+		this.clientSocket.setSoTimeout(clientHandler.timeout);
+		this.clientSocket.setTcpNoDelay(true);
+		
+		// zero out the buffer from its last use
+		for (int i=0; i<BUF_SIZE;i++) {
+			buf[i] = 0;
+		}
+		try {
+			int nread = 0, r = 0;
+outerloop:
+			while(nread < BUF_SIZE) {
+				r = inputStream.read(buf, nread, BUF_SIZE - nread);
+				if ( r == -1) {
+					// EOF
+					return;
+				}
+				int i = nread;
+				nread += r;
+				for (; i<nread; i++) {
+					if (buf[i] == (byte)'\n' || buf[i] == (byte)'\r') {
+						//read one line
+						break outerloop;
+					}
+				}
+			}
+			
+			// figure out which HTTP method we are doing, either GET or HEAD
+			boolean doingGet;
+			
+			if (buf[0] == (byte)'G' &&
+					buf[1] == (byte)'E' &&
+					buf[2] == (byte)'T' &&
+					buf[3] == (byte)' ') {
+				doingGet = true;
+				//index = 4;
+			}
+			
+		} finally {
+			
+		}
+		
+	}
 }
