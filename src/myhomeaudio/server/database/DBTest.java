7eb2cac155bec2eb9eec38766cbaeb198ff624e1
camporter <camporter1@gmail.com>
2011-10-09 18:11:23 -0500

    Db testing...

---
+++ src/myhomeaudio/server/database/DBTest.java
@@ -0,0 +1,13 @@
+package myhomeaudio.server.database;
+
+public class DBTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Database db = new Database("db");
+	}
+
+}
