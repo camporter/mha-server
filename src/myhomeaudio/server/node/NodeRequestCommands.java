e575c0018b3e967fae4694523d58949cbfa9ff82
camporter <camporter1@gmail.com>
2011-10-11 12:47:27 -0500

    Tweaks

---
+++ src/myhomeaudio/server/node/NodeRequestCommands.java
@@ -0,0 +1,10 @@
+package myhomeaudio.server.node;
+
+public interface NodeRequestCommands {
+	final static int INIT = 0;
+	final static int PLAY = 1;
+	final static int SWITCH = 2;
+	final static int STOP = 3;
+	final static int DISCONNECT = 4;
+	final static int RECEIVED = 5;
+}
