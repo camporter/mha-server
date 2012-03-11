91bf418fe33de1d6e4d6d54fcc774cd7e6a01889
Ryan <rcbrown2.r@gmail.com>
2012-03-11 13:55:12 -0500

    Changed visibility of getClient from ClientManager to public Started locations check within ClientHelper

--- src/myhomeaudio/server/http/helper/ClientHelper.java
+++ src/myhomeaudio/server/http/helper/ClientHelper.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 import java.util.StringTokenizer;
 
 import myhomeaudio.server.client.Client;
+import myhomeaudio.server.database.object.DatabaseClient;
 import myhomeaudio.server.http.HTTPMimeType;
 import myhomeaudio.server.http.StatusCode;
 import myhomeaudio.server.manager.ClientManager;
@@ -86,6 +87,13 @@ public class ClientHelper extends Helper implements HelperInterface, NodeCommand
 			} else if (uriSegments.get(1).equals("logout")) {
 				System.out.println("Getting logout from client");
 
+			} else if (uriSegments.get(1).equals("locations")){
+				if (jsonRequest.containsKey("session") && jsonRequest.containsKey("locations")){
+					DatabaseClient dClient = cm.getClient((Integer)jsonRequest.get("session"));
+					
+					body.put("status", STATUS_OK);
+					this.httpStatus = HttpStatus.SC_OK;
+				}	
 			}
 		} catch (Exception e) {
 			// Do nothing for now
