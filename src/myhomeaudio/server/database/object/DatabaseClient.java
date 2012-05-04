bfd5ae42e23cd3c86c44c232cbc0dba09a20d9d7
Ryan <rcbrown2.r@gmail.com>
2012-05-03 19:32:39 -0500

    Bug Fix Null pointer exception

--- src/myhomeaudio/server/database/object/DatabaseClient.java
+++ src/myhomeaudio/server/database/object/DatabaseClient.java
@@ -145,7 +145,7 @@ public class DatabaseClient extends DatabaseObject<Client> {
 	public JSONObject getJSON(){
 		JSONObject object = new JSONObject();
 		object.put("id", loggedUserId);
-		object.put("session", sessionId.subSequence(0, 7));
+		object.put("session", sessionId != null ? sessionId.subSequence(0, 7) : null);
 		object.put("closedNode", closestNode != null ? closestNode.getName() : null);
 		object.put("isConfigured",nodeSignatures != null);
 		return object;
